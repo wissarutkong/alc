b3207b0ae32c9f44a27f2d5cd172dc5650ff9d26
wissarutfirst <firstwantup@gmail.com>
2022-08-23 22:57:51 +0700

    add logger_info temp

---
+++ logger_info.php
@@ -0,0 +1,25 @@
+<?php
+
+require_once 'service/db/cons.php';
+
+$query1 = $_GET['query'];
+$buffgsm = explode(",", $query1);
+
+$DataLogger_Id = $buffgsm[0];
+$Logger_Brand = $buffgsm[1];
+$Logger_model = $buffgsm[2];
+$gsm_module = $buffgsm[3];
+$gsm_firmware_version = $buffgsm[4];
+$ip_address = $buffgsm[5];
+$iccid = $buffgsm[6];
+$operator = $buffgsm[7];
+$latitude = $buffgsm[8];
+$logtitude = $buffgsm[9];
+$option = $buffgsm[10];
+$dt = date("Y-m-d H:i:s");
+
+echo "@>None#";
+echo ",";
+echo "$dt";
+echo ",";
+echo "#";
