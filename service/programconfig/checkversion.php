39f1a662ea480305b88356da8ae764794ea10e83
wissarutfirst <firstwantup@gmail.com>
2022-10-12 00:43:27 +0700

    programconfig update

--- service/programconfig/checkversion.php
+++ service/programconfig/checkversion.php
@@ -1,1 +1,1 @@
-<?php echo "4.0.0.2,- เพิ่ม Model Rs485 สามารถเลือก Other กรอกค่าเอง,- เพิ่ม Text Version ในหน้าโปรแกรม,- เพิ่ม Text สถานะเมื่อมีการ Upload";
+<?php echo "4.0.0.3,- เพิ่ม Model Rs485 สามารถเลือก Other กรอกค่าเอง,- เพิ่ม Text Version ในหน้าโปรแกรม,- เพิ่ม Text สถานะเมื่อมีการ Upload";
