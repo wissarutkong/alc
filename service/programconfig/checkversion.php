e1fae4a171c879e324dcd44ff86b1bc6fbe9335c
wissarutfirst <firstwantup@gmail.com>
2023-03-23 13:09:25 +0700

    programconfig add loading home and edit size scroll

--- service/programconfig/checkversion.php
+++ service/programconfig/checkversion.php
@@ -1,1 +1,1 @@
-<?php echo "4.0.0.16,- เพิ่ม STV3,- เพื่อโหมด Control ใน STV3,- ปรับขนาดการแสดงผลใน STV3,- เพิ่มฟังก์ชั่น detect auto comport หน้า Home";
+<?php echo "4.0.0.17,- เพิ่ม STV3,- เพื่อโหมด Control ใน STV3,- ปรับขนาดการแสดงผลใน STV3,- เพิ่มฟังก์ชั่น detect auto comport หน้า Home,- เพิ่ม Loading หน้า Home";
