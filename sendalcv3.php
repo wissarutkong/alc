c1d8f3fc4a4afc9574fdc450013c7f1fc45a6f50
wissarutfirst <firstwantup@gmail.com>
2023-03-25 14:14:19 +0700

    edit sendalc3

--- sendalcv3.php
+++ sendalcv3.php
@@ -14,24 +14,25 @@ $query1 = $_GET['query'];
 $buffgsm = explode(",", $query1);
 
 $dt = date("Y-m-d H:i:s");
-$id_name = $buffgsm[0]; 						// DataLoggerId
-$Ai1 = floatval($buffgsm[1]); 					// Pressure1
-$Ai2 = floatval($buffgsm[2]); 					// Pressure2
-$io5_counter = floatval($buffgsm[5]);				//Flow forward insert into dma_last field io5_counter
-$io5_acc = floatval($buffgsm[11]);			//Flow Total forward into dma_last field io5_acc
-$io3 = floatval($buffgsm[15]);			//Temp on board insert into dma_last field io3
-$CSQ = floatval($buffgsm[16]);					//Signal Gsm 
-$VBATT = floatval($buffgsm[17]);				//Battery Voltage
-$SolarVoltage = floatval($buffgsm[18]);			//SolarVoltage
-$dtd_now = $buffgsm[19]; 	
+$id_name = $buffgsm[0];                         // DataLoggerId
+$Ai1 = floatval($buffgsm[1]);                     // Pressure1
+$Ai2 = floatval($buffgsm[2]);                     // Pressure2
+$io5_counter = floatval($buffgsm[5]);                //Flow forward insert into dma_last field io5_counter
+$io5_acc = floatval($buffgsm[11]);            //Flow Total forward into dma_last field io5_acc
+$io3 = floatval($buffgsm[15]);            //Temp on board insert into dma_last field io3
+$CSQ = floatval($buffgsm[16]);                    //Signal Gsm 
+$VBATT = floatval($buffgsm[17]);                //Battery Voltage
+$SolarVoltage = floatval($buffgsm[18]);            //SolarVoltage
+$dtd_now = $buffgsm[19];
 $dtd = DateTime::createFromFormat('YmdHis', $dtd_now)->format('Y-m-d H:i:s');  // Date Logger To SQL DB
-$fw_version = $buffgsm[20];				//fw version new field 
+$fw_version = $buffgsm[20];                //fw version new field 
 
 
 echo "@>None#";
 echo ",";
 echo "$dt";
 echo "#";
+
 
 try {
     $conmand = $conn->prepare("INSERT INTO realtime_data_1 (id_name,datetime,dtd,p_out,p_in,flowrate,flowtotal,CSQ,VBATT,SolarVoltage,temperature,fw_version) 
@@ -65,10 +66,13 @@ try {
 
     publish_mqtt($topic, $message);
 
-    // $response = [
-    //     'status' => true,
-    //     'message' => 'success'
-    // ];
+
+    $response = array(
+        'success' => true,
+        'unixtime' => strtotime($dt)
+    );
+    echo json_encode($response);
+
     http_response_code(200);
     // echo json_encode($response);
 } catch (Exception $e) {
